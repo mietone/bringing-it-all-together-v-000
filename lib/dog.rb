d304fc6e0267a15536cd4db7b5638ccca6e6d07b
Miwa <paintfx@gmail.com>
2018-02-05 14:53:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,7 +24,11 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(name, breed, id)
   end
 
 
